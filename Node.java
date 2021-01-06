e9ca0cb1e977aeabbd0d0fe9d6fbe9aab17d0060
Yuqing Wu <ywu20@stuy.edu>
2021-01-06 13:06:50 -0500

    initial commit

---
+++ Node.java
@@ -0,0 +1,6 @@
+public class Node{
+ public Node(String value){/*create a constructor*/}
+ private String data;
+ private Node next,prev;
+ //write get/set methods for all three instance variables.
+}
