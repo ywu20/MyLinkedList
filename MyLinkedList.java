9cd8ff2a91dedc6d7738f7e6b52960e5f3f8d474
Yuqing Wu <ywu20@stuy.edu>
2021-01-07 22:31:39 -0500

    constructor, add to end, size

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,39 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
+ public MyLinkedList(){
+   size=0;
+   start=new Node("");
+   end=new Node("");
+ }
+ public int size(){
+   return size;
+ };
+ public boolean add(String value){
+   Node x=new Node(value);
+   Node y=new Node(end.getData());
+   if(size==0){
+     start.setData(value);
+   }
+ else if (size==1){
+   start.setNext(x);
+ }else{
+   end.setPrev(y);
+ }
+ end.setData(value);
+ size++;
+   return true;
+ };
+
+ /*
+ public void add(int index, String value){
+ };
  public String get(int index);
  public String set(int index, String value);
- public String toString();
+ */
+ public String toString(){
+   return start.toString()+" "+end.toString(); //just for preliminary testing
+ };
  //Any helper method that returns a Node object MUST BE PRIVATE!
+
 }
